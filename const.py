5ba25d8c5647065ccb67ce10efa81598b5e9a99a
MrUncle <50102399+MrUncle@users.noreply.github.com>
2019-04-30 12:59:27 +0700

    lib

---
+++ const.py
@@ -0,0 +1,39 @@
+# Date: 12/28/2018
+# Author: MrTamfanX / MrUncle
+# Description: Constants
+
+# Browser
+header = {
+    'connection': 'close',
+    'accept-language': 'en-US,en;q=0.9',
+    'accept-encoding': 'gzip, deflate, br',
+    'referer': 'https://www.instagram.com/',
+    'content-type': 'application/x-www-form-urlencoded'
+}
+
+username_field = 'username'
+password_field = 'password'
+home_url = 'https://www.instagram.com/'
+login_url = 'https://www.instagram.com/accounts/login/ajax/'
+
+browser_data = {
+	'header': header,
+	'home_url': home_url, 
+	'login_url': login_url,
+	'username_field': username_field,
+	'password_field': password_field
+}
+
+# Login
+fetch_time = (5, 10)
+response_codes = { 'succeed': 0, 'failed': 1, 'locked': -1 }
+
+# Limits
+max_bad_proxies = 256
+max_time_to_wait = 10
+max_bots_per_proxy = 16
+
+# Misc
+debug = False
+credentials = 'accounts.txt'
+modes = { 0: 256, 1: 128, 2: 64, 3: 32 } 
---
+++ session.py
@@ -0,0 +1,39 @@
+# Date: 05/05/2018
+# Author: MrUncle / MrTamfanX
+# Description: Session Handler
+
+from os import remove
+from os.path import exists as path 
+from csv import DictWriter, DictReader
+
+
+class Session(object):
+
+    def __init__(self, username, passlist):
+        self.file = '.{}_{}.csv'.format(username, passlist.replace('\\', '_').replace('/', '_'))
+
+    @property 
+    def exists(self):
+        return path(self.file)
+
+    def read(self):
+        with open(self.file, 'rt') as csvfile:
+            session = DictReader(csvfile, delimiter = ',')
+            try:return [_ for _ in session][0]
+            except:pass 
+
+    def write(self, attempts, queue):
+        if not attempts:return
+        with open(self.file, 'w') as csvfile:
+            fieldnames = ['attempts', 'list']
+            writer = DictWriter(csvfile, fieldnames=fieldnames)
+
+            writer.writeheader()
+            writer.writerow({ 'attempts': attempts, 'list': queue })
+
+    def delete(self):
+        if self.exists:
+            try:
+                remove(self.file) 
+            except:
+                pass
